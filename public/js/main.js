6bd487e0423589844ee2df2befa091f4f6eda602
allyrods <alistair.rodrigues@gmail.com>
2014-08-04 12:27:49 +0530

    Email validations

--- public/js/main.js
+++ public/js/main.js
@@ -8,9 +8,58 @@ $(document).ready(function() {
 	$('.members .cover').height(winH).width(winW);
 	$('#page-bg').anystretch();
 
+	var form = $('#memberenroll');
 
+	var email = $('#eadd');
+	var emailInfo = $('#emailInfo');
+	var eaddgrp = $('#eaddgrp');
 	
-	
+	form.submit(function() {
+		if (validateEmail()) {
+			return true;
+		} else {
+			return false;
+		}
+	});
+
+	function validateEmail() {
+        //validation for empty emails
+        if (email.val() == '') {
+            email.addClass('has-error');
+            eaddgrp.addClass('has-error');
+            emailInfo.addClass('has-error');
+            emailInfo.text('Email cannot be empty!');
+            return false;
+        } else {
+            email.removeClass('has-error');
+            eaddgrp.removeClass('has-error');
+            emailInfo.removeClass('has-error');
+            emailInfo.text('');
+        }
+ 
+        //validation for proper email formats
+        //testing regular expression
+        var a = email.val();
+ 		alert (a);
+        var filter = /^[a-zA-Z0-9]+[a-zA-Z0-9_.-]+[a-zA-Z0-9_-]+@[a-zA-Z0-9]+[a-zA-Z0-9.-]+[a-zA-Z0-9]+.[a-z]{2,4}$/;
+        //if it's valid email
+        if (filter.test(a)) {
+            email.removeClass('has-error');
+            eaddgrp.removeClass('has-error');
+            emailInfo.removeClass('has-error');
+            emailInfo.text('');
+            return true;
+        }
+        //if it's NOT valid
+        else {
+            email.addClass('has-error');
+            emailInfo.addClass('has-error');
+            eaddgrp.addClass('has-error');
+            emailInfo.text('Enter Valid E-mail please..!');
+            return false;
+        }
+    }
+
 	// var $homeP = $('.home-books-block');
 	
 	// $homeP.waypoint(function(direction) {
