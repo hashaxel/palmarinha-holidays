ab15b054ddf3b0c90cdffe1932562cc41a2be378
Milind <milind@hashcooki.es>
2014-09-01 06:22:05 +0530

    Writing styles etc

--- public/js/main.js
+++ public/js/main.js
@@ -8,56 +8,10 @@ $(document).ready(function() {
 	$('.members .cover').height(winH).width(winW);
 	$('#page-bg').anystretch();
 	
-	// var $homeP = $('.home-books-block');
-	
-	// $homeP.waypoint(function(direction) {
-	// 	if (direction == "down") {
-	// 		clearInterval(timeout);
-	// 	}
-	// }, { offset: '25%' });
-	
-	// $homeP.waypoint(function(direction) {
-	// 	if (direction == "up") {
-	// 		runCarousel();
-	// 	}
-	// }, { offset: '25%' });
-	
-	// setTimeout(function() {
-	// 	$('body').addClass('loaded');
-	// 	$car.find('li').not('.left').addClass('animated');
+	$('.home-controls').css({
+		top: ((winH - 260) / 2) + 160
+	});
 		
-	// 	runCarousel();
-	// }, 5000)
-	
-	// $('.scrollto').click(function() {
-	// 	var id = $(this).attr('href');
-		
-	// 	$.scrollTo(id, 1200);
-	// 	return false;
-	// });
-	
-	// $sesProd = $('.previously-viewed');
-	// $prodCart = $('#books-cart');
-	// $sesProd.find('li').click(function() {
-	// 	var val = $(this).text();
-	// 	var prodVal = $prodCart.val();
-	// 	$prodCart.val(prodVal + ', ' + val);
-		
-	// 	$(this).remove();
-		
-	// 	return false;
-	// });
-	
-	// var $prodGrid = $('#book-grid');
-	
-	// $prodGrid.find('li').click(function() {
-	// 	var url = $(this).find('h3 a').attr('href');
-		
-	// 	window.location.href = url;
-	// });
-	
-	// $('.file-inputs').bootstrapFileInput();
-	
 	var $delBtn = $('.delete-btn');
 	var $delModal = $('.delete-modal');
 	
