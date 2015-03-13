2b65431d76602de36b6dc3b652cbd2ad5a99d07a
Ravi Siddarth <ravisiddarth13@gmail.com>
2015-03-13 16:10:54 +0530

    mobile layout fixes

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
 	$('.home .cover').height(winH).width(winW);
 	$('.members-page .cover').height(winH).width(winW);
 	$('#page-bg').anystretch();
-	$('#login-btn').insertBefore('#more-info-btn')
+	if(Modernizr.mq('(max-width: 767px)')) {
+		$('#login-btn').insertBefore('#more-info-btn');
+	}
 
 	//email validation code starts
 	var form = $('#memberenroll');
